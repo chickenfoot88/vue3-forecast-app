4c12ab7f929d1c645136bb8de82ae0e56e9dc9eb
chickenfoot88 <chikenfoot88@gmail.com>
2025-08-27 13:45:05 -0300

    add App component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <script setup>
+import BaseButton from './components/BaseButton.vue';
 </script>
 
 <template>
+  <main class="main">
+    <BaseButton>
+      Сохранить
+    </BaseButton>
+  </main>
 </template>
 
 <style scoped>
+  .main {
+    background: var(--color-background-main);
+    padding: 60px 50px;
+    border-radius: 25px;
+  }
+
 </style>
