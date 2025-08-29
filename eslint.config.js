9c794733902912c9b55ef6fce1535f95cb39c6fa
chickenfoot88 <chikenfoot88@gmail.com>
2025-08-29 09:54:58 -0300

    add esling browser env

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import vue from "eslint-plugin-vue";
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
@@ -11,6 +12,9 @@ export default [
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
     },
   },
 ];
