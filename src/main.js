f72cb1cee3ff7ab943b6360fdae28781568f5242
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-03 07:25:04 -0300

    add global directive focus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const application = createApp(App);
+application.directive("focus", {
+  mounted: (el) => el.focus(),
+});
+application.mount("#app");
