de33c15b2554b6492db81c30424d565a75f2b54f
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-04 07:44:20 -0300

    add error message component

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,26 @@
+<script setup>
+  const props = defineProps({
+    errorMessage: {
+      type: String,
+      default: '',
+    },
+  })
+
+</script>
+
+<template>
+  <div class="error-message">
+    {{ props.errorMessage }}
+  </div>
+</template>
+
+
+<style scoped>
+  .error-message {
+    font-size: 18px;
+    padding: 15px;
+    text-align: center;
+    background: var(--color-background-card);
+    border-radius: 0px 0px 25px 25px;
+  }
+</style>
